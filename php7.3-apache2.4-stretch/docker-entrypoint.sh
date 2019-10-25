feb6ad9bf5c081eba16e493733942bcee10aa243
dmh <dmytro.hrynevych@gmail.com>
2019-10-25 16:10:32 +0300

    [TASK] add php7.3-apache2.4-stretch image

---
+++ php7.3-apache2.4-stretch/docker-entrypoint.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+if [[ -n $VIRTUAL_HOST ]]
+then
+    # Add VIRTUAL HOST to hosts file on web container
+    if grep -q "$VIRTUAL_HOST" /etc/hosts
+    then
+        echo "$VIRTUAL_HOST already exists"
+    else
+        echo "127.0.0.1 ${VIRTUAL_HOST}" >> /etc/hosts
+        echo "$VIRTUAL_HOST added succesfully"
+    fi
+fi
+
+# # set volume permissions
+if [[ -d /var/www/html/var/ ]]
+then
+    chown www-data /var/www/html/var/
+fi
+if [[ -d /var/www/html/public/typo3temp/ ]]
+then
+    chown www-data /var/www/html/public/typo3temp/
+fi
+
+exec "$@"
