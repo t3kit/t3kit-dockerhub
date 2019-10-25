6fbef7fa956621c3ee6ffc618c4ae60cb3dd5e05
dmh <dmytro.hrynevych@gmail.com>
2019-10-25 17:23:47 +0300

    [TASK] add entrypoint for ubuntu18.04/php images

--- ubuntu18.04/php7.2-apache2.4/docker-entrypoint.sh
+++ ubuntu18.04/php7.2-apache2.4/docker-entrypoint.sh
@@ -3,6 +3,21 @@
 set -e
 set -o pipefail
 
+# Make www-data user use id from hosts user to make shared folder writable. (!!!Only for Ubuntu)
+if [ -z "$USER_ID" ]
+then
+    echo "USER_ID variable is not set."
+else
+    # fetch current userid and groupid for user www-data
+    WWW_DATA_USERID=$(id -u www-data)
+    # if current userid doesn't equal the one from .env, force id of user to the one from .env
+    if [ "$WWW_DATA_USERID" -ne "${USER_ID}" ];then
+        echo "usermod -u ${USER_ID} www-data"
+        usermod -u "${USER_ID}" www-data
+    fi
+fi
+echo id www-data
+
 # Add VIRTUAL HOST to hosts file on web container
 if [[ -n $VIRTUAL_HOST ]]
 then
