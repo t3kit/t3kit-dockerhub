d1ec2f989c18d604a23ebdaa9d50b10471863196
dmh <dmh@users.noreply.github.com>
2021-10-29 10:46:21 +0300

    [TASK] update nproxy, check if directory nproxy/certs exists

--- nproxy/docker-entrypoint.sh
+++ nproxy/docker-entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 set -o pipefail
 
 # copy SSL certificates from container folder to volume folder to spread it between all containers inside nproxy docker network using shared volume
-if [[ -d /var/www/html/var/ ]]
+if [[ -d /nproxy/certs/ ]]
 then
-    cp -r /nproxy/certs/* /etc/nginx/certs/
+    cp -r /nproxy/certs/. /etc/nginx/certs/
 fi
 
 # shellcheck disable=SC1091
